71b244e587ee7c2cb8d474a2ba15c20e4489ef40
ShimuSourav <rdfox.007@gmail.com>
2019-11-05 19:05:15 +0600

    adding more files

---
+++ NewOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 4;
+
+            Console.WriteLine(num1 * num2);
+
+            Console.ReadKey();
+        }
+        
+    }
+}
